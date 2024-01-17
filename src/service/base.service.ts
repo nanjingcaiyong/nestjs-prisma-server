233849512d53eab4c85b9e0613c2041593c23480
caiyong <yong.cai@kapeixi.com>
2024-01-17 18:59:38 +0800

    feat: 添加环境配置

--- src/service/base.service.ts
+++ src/service/base.service.ts
@@ -5,6 +5,7 @@ import { PrismaClient } from '@prisma/client';
 export class BaseService extends PrismaClient implements OnModuleInit {
   constructor() {
     super({
+      datasourceUrl: process.env.DATABASE_URL,
       log: ['query', 'info', 'warn', 'error'],
     });
   }
