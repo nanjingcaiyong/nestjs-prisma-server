a693b92e755cae01e8173b9ecccedaa2b8052d87
caiyong <yong.cai@kapeixi.com>
2024-03-22 09:22:38 +0800

    feat: seed.ts 数据

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,19 +3,62 @@ import { crypto } from '../src/utils/crypto';
 
 const prisma = new PrismaClient({});
 
-function createAdminRole() {
+/**
+ * @description 创建管理员账号
+ * @returns
+ */
+async function createAdminAccount() {
   return prisma.account.create({
     data: {
       account: 'admin',
       password: crypto.encodePassword('admin123'),
       email: 'yong.cai@kapeixi.com',
       roles: {
-        create: [
+        connectOrCreate: [
           {
-            name: 'admin',
-            creator: 'caiyong',
+            where: { roleName: 'admin' },
+            create: {
+              roleName: 'admin',
+              creator: 'caiyong',
+              auths: {
+                create: [
+                  {
+                    authName: 'visit',
+                    type: 0,
+                    desc: '查询接口操作权限',
+                  },
+                  {
+                    authName: 'add',
+                    type: 1,
+                    desc: '新增接口操作权限',
+                  },
+                  {
+                    authName: 'delete',
+                    type: 2,
+                    desc: '删除接口操作权限',
+                  },
+                  {
+                    authName: 'update',
+                    type: 3,
+                    desc: '更新接口操作权限',
+                  },
+                  {
+                    authName: 'all',
+                    type: 99,
+                    desc: '超级管理员权限',
+                  },
+                ],
+              },
+            },
           },
         ],
+      },
+    },
+    include: {
+      roles: {
+        include: {
+          auths: true,
+        },
       },
     },
   });
@@ -24,9 +67,7 @@ function createAdminRole() {
 async function main() {
   await prisma.story.deleteMany();
   console.log('Seeding...');
-
-  const res = await createAdminRole();
-
+  const res = await createAdminAccount();
   console.log('===>', res);
 }
 
